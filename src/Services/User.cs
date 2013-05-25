a8c5c6db0cc99617eebec325f1192ff4f8526787
Artem Smirnov <uluhonolulu@gmail.com>
2013-05-25 14:38:05 +0300

    Upgraded template to Fubu.1

--- src/Services/User.cs
+++ src/Services/User.cs
@@ -1,9 +1,7 @@
-namespace $safeprojectname$.Services
-{
-    public class User
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
+namespace $safeprojectname$.Services {
+	public class User {
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+	}
 }
--- src/UI/AppHtmlConventions.cs
+++ src/UI/AppHtmlConventions.cs
@@ -1,17 +1,14 @@
 ﻿using FubuMVC.Core.UI;
 using HtmlTags;
 
-namespace $safeprojectname$.UI
-{
-    public class AppHtmlConventions : HtmlConventionRegistry
-    {
-        public AppHtmlConventions()
-        {
-            Editors.IfPropertyIs<bool>()
-                .BuildBy(e => new CheckboxTag(e.Value<bool>()));
+namespace $safeprojectname$.UI {
+	public class AppHtmlConventions : HtmlConventionRegistry {
+		public AppHtmlConventions() {
+			Editors.IfPropertyIs<bool>()
+				.BuildBy(e => new CheckboxTag(e.Value<bool>()));
 
-            Editors.If(x => x.Accessor.Name.Contains("Password"))
-                .BuildBy(e => new TextboxTag(e.ElementId, e.StringValue()).Attr("type", "password"));
-        }
-    }
+			Editors.If(x => x.Accessor.Name.Contains("Password"))
+				.BuildBy(e => new TextboxTag(e.ElementId, e.StringValue()).Attr("type", "password"));
+		}
+	}
 }
